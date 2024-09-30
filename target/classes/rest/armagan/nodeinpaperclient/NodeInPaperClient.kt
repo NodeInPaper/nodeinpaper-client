847bd2f8501a84b743ff2b7ebc1ee9e55617416e
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2024-09-30 17:35:18 +0300

    Initial commit

---
+++ target/classes/rest/armagan/nodeinpaperclient/NodeInPaperClient.kt
@@ -0,0 +1,14 @@
+package rest.armagan.nodeinpaperclient
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NodeInPaperClient : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
