74d513563017131d8a3b0225b0e056af91554068
Kıraç Armağan Önal <kiracarmaganonal@gmail.com>
2024-10-01 13:43:15 +0300

    action processor

---
+++ src/main/kotlin/rest/armagan/nodeinpaperclient/DataTypes.kt
@@ -0,0 +1,3 @@
+package rest.armagan.nodeinpaperclient
+
+data class Action(val key: String, val type: String, val args: List<Any>? = null)
--- src/main/kotlin/rest/armagan/nodeinpaperclient/NodeInPaperClient.kt
+++ src/main/kotlin/rest/armagan/nodeinpaperclient/NodeInPaperClient.kt
@@ -1,14 +1,51 @@
 package rest.armagan.nodeinpaperclient
 
 import org.bukkit.plugin.java.JavaPlugin
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.memberProperties
 
 class NodeInPaperClient : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+
+
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
     }
+
+    fun processActions(obj: Any, actions: List<Action>): Any? {
+        var currentObj: Any? = obj
+
+        for (action in actions) {
+            if (currentObj == null) {
+                throw IllegalArgumentException("Invalid object state during processing: ${action.key}")
+            }
+
+            when (action.type) {
+                "Get" -> {
+                    val property = currentObj::class.memberProperties.firstOrNull { it.name == action.key }
+                    if (property != null) {
+                        currentObj = (property as KProperty1<Any, *>).get(currentObj)
+                    } else {
+                        throw IllegalArgumentException("No property found with name: ${action.key}")
+                    }
+                }
+                "Apply" -> {
+                    val function = currentObj::class.members.find { it.name == action.key && it.parameters.size == (action.args?.size?.plus(1) ?: 1) }
+                    if (function != null) {
+                        currentObj = function.call(currentObj, *(action.args?.toTypedArray() ?: arrayOf()))
+                    } else {
+                        throw IllegalArgumentException("No function found with name: ${action.key}")
+                    }
+                }
+                else -> throw IllegalArgumentException("Unknown action type: ${action.type}")
+            }
+        }
+
+        return currentObj;
+    }
+
 }
